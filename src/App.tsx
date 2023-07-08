f598225b5901a94d9515dcf9f8ec47b8006d7ea2
gilmujjang <40172373+gilmujjang@users.noreply.github.com>
2023-07-08 15:39:03 +0900

    [dev] feat: firebase google auth 연동
    구글 로그인 연동

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,24 @@ import "./App.css";
 import Datas from "./Pages/Datas";
 import Header from "./components/Header";
 import { Routes, Route } from "react-router-dom";
+import { auth } from "./Firebase";
+import { useState } from "react";
+import { GoogleAuthProvider, User, signInWithPopup } from "firebase/auth";
 
 const App = () => {
+  const [userData, setUserData] = useState<User | null>(null);
+
+  const handleGoogleLogin = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((data) => {
+        setUserData(data.user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div
       style={{
@@ -13,6 +29,7 @@ const App = () => {
       }}
     >
       <Header />
+      <button onClick={handleGoogleLogin}>Login</button>
       <Routes>
         <Route path="/" element={<Datas />} />
       </Routes>
